ca5a9be6a10e0ac51d8e740da29fdef7b113acf9
MelomanCode <dmitryKezik@gmail.com>
2023-09-01 14:06:14 +0200

    v.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { UserComponent } from './user/user.component';
 import { NgbDatepickerModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {PersonalAreaForPracticeComponent} from "./personal-area/personal-area-for-practice.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, UserComponent],
+  declarations: [AppComponent, HomeComponent, UserComponent, PersonalAreaForPracticeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/personal-area/personal-area-for-practice.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-area',
+  templateUrl: './personal-area-for-practice.component.html',
+  styleUrls: ['./personal-area-for-practice.component.css'],
+})
+export class PersonalAreaForPracticeComponent {
+
+
+}
